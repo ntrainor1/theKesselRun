daff99106f8f118baadeacb42ee2520e34a829a6
AndrewD <adufresplatinum@gmail.com>
2018-06-04 13:39:47 -0600

    fix my behind

--- swREST/src/main/java/com/skilldistillery/swapp/service/AuthServiceImpl.java
+++ swREST/src/main/java/com/skilldistillery/swapp/service/AuthServiceImpl.java
@@ -2,11 +2,13 @@ package com.skilldistillery.swapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.skilldistillery.swapp.User;
 import com.skilldistillery.swapp.repository.UserRepo;
 
+@Service
 public class AuthServiceImpl implements AuthService {
 	@Autowired
 	private UserService userService;
