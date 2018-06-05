b907cec86e8bf7f112a4333dd3e85354259722c6
AndrewD <adufresplatinum@gmail.com>
2018-06-05 08:17:29 -0600

    updateuser

--- ngKesselRun/src/app/profile/profile.component.ts
+++ ngKesselRun/src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { UserService } from './../user.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -14,9 +15,17 @@ export class ProfileComponent implements OnInit {
 
 
   updateUser(user) {
-    this.userService.update(user);
+    this.userService.update(user)
+    // .subscribe(
+    //   data => {
+    //     console.log(user);
+    //     this.router.navigateByUrl('profile');
+    //   },
+    //   err => console.log(err)
+    // )
+    ;
   }
-  constructor(private userService: UserService, private home: HomeComponent) { }
+  constructor(private router: Router, private userService: UserService, private home: HomeComponent) { }
 
   ngOnInit() {
     this.user = this.home.returnUser();
